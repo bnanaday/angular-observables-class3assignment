1b832e2704ab35656f0cfa304800d92c7d536d41
bnanaday <bnanaday@gmail.com>
2022-04-13 11:56:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-observables-rxjs-kyyv3s
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-observables-rxjs-kyyv3s)
---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { map, filter, take, reduce } from 'rxjs/operators';
+import { interval, pipe } from 'rxjs';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  constructor() {}
+  //check the app for notes on these rxjs operators and make sure to open the console on developer tools
+  ngOnInit() {
+    interval(1000)
+      .pipe(
+        take(10),
+        filter((x) => x % 2 == 0),
+        map((y) => y * 10)
+      )
+      .subscribe((data) => console.log(data));
+
+    // interval(1000)
+    //   .pipe(
+    //     take(5),
+    //     filter((x) => x % 2 == 0),
+    //     map((v) => Date.now())
+    //   )
+    //   .subscribe((value) => console.log('Subscriber: ' + value));
+  }
+}
